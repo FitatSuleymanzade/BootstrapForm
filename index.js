2dc445e621cc3debb57dc2eed2e2bf879920502e
Fitat Suleymanzade <146056401+FitatSuleymanzade@users.noreply.github.com>
2023-11-23 01:31:38 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,96 @@
+const submit=document.getElementById('myFORM')
+
+
+
+
+
+
+// Name,Surname,Age,Email
+
+// const nameinput = document.getElementById('nameinput');
+// const surnameinput = document.getElementById('surnameinput');
+// const ageinput = document.getElementById('ageinput');
+// const emailinput = document.getElementById('emailinput');
+
+
+// function axiosPost(event) {
+//     event.preventDefault()
+//     axios.post("https://northwind.vercel.app/api/customers", {
+//         name: nameinput.value,
+//         surname: surnameinput.value,
+//         age: ageinput.value,
+//         email: emailinput.value
+//     }).then(res => {
+//         console.log(res);
+//     })
+// }
+// submit.addEventListener('submit', axiosPost)
+
+
+
+
+
+
+
+
+
+// Username,Email,Password,Addres,country
+
+// const usernameinput=document.getElementById('usernameinput');
+// const passwordinput=document.getElementById('passwordinput');
+// const addressinput=document.getElementById('addressinput');
+// const countryinput=document.getElementById('countryinput');
+
+
+// function axiosPost(event) {
+//     event.preventDefault()
+//     axios.post("https://northwind.vercel.app/api/customers", {
+//         username: usernameinput.value,
+//        email: emailinput.value,
+//         password: passwordinput.value,
+//         address: addressinput.value,
+//        country: countryinput.value
+//     }).then(res => {
+//         console.log(res);
+//     })
+// }
+// submit.addEventListener('submit', axiosPost)
+
+
+
+
+
+
+
+
+// Tile, Price, Description, Discount, Stock, Quality, Country ,СustomersCount, Category, City
+
+// const titleinput=document.getElementById('titleinput');
+// const descriptioninput=document.getElementById('descriptioninput');
+// const discountinput=document.getElementById('discountinput');
+// const stockinput=document.getElementById('stockinput');
+// const qualityinput=document.getElementById('qualityinput');
+// const customerscountinput=document.getElementById('countinput');
+// const categoryinput=document.getElementById('categoryinput');
+// const cityinput=document.getElementById('cityinput');
+// const priceinput=document.getElementById('priceinput');
+
+
+// function axiosPost(event) {
+//     event.preventDefault()
+//     axios.post("https://northwind.vercel.app/api/customers", {
+//         title: titleinput.value,
+//        price: priceinput.value,
+//         description: descriptioninput.value,
+//        discount: discountinput.value,
+//        stock: stockinput.value,
+//        quality :qualityinput.value,
+//        country:countryinput.value,
+//        customerscount:customerscountinput.value,
+//        category:categoryinput.value,
+//        city:cityinput.value
+//     }).then(res => {
+//         console.log(res);
+//     })
+// }
+// submit.addEventListener('submit', axiosPost)
